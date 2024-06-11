0002f9ea4e90b4d1a63a98d813229110a360d130
Florent Lavignotte <florent.lavignotte@gmail.com>
2024-06-11 08:22:26 +0200

    Update appComponent to disable header menu if the user is not logged

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,7 +1,9 @@
 
-import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { DatePipe, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AccountService } from './_services/account.service';
+import { User } from './_interfaces/user';
 
 @Component({
     selector: 'app-root',
@@ -9,12 +11,25 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     imports: [
         RouterOutlet,
         RouterLink,
-        DatePipe
+        DatePipe,
+        NgIf
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
+    private accountService: AccountService = inject(AccountService);
+
+    public user?: User | null;
+
+    constructor() {
+        this.accountService.user.subscribe(user => this.user = user);
+
+        if (this.accountService.getUserValue()) {
+            this.user = this.accountService.getUserValue();
+        }
+    }
+    
     title: string = 'Quotidian';
     date: Date = new Date();
 }
